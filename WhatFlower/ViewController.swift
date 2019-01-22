c824af8e25c10b8998ff4eeaba6f7e704c8ec08e
Aleksandr Skorotkin <killko@MacBook-Pro-Aleksandr.local>
2019-01-22 12:53:59 +0700

    Initial commit from git

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Aleksandr Skorotkin on 22/01/2019.
+//  Copyright © 2019 Aleksandr Skorotkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
